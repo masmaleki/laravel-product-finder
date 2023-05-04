79f2cb4fa39c50150488e6e0454c5c6061543dde
davood.thinker <davood.thinker@gmail.com>
2023-05-04 09:52:56 +0330

    add model

---
+++ src/Models/PFWizard.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Masmaleki\LaravelProductFinder\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PFWizard extends Model
+{
+    use HasFactory;
+
+   
+    protected $table = 'pf_wizards';
+
+    protected $guarded = [];
+
+    protected $casts = [
+        
+    ];
+
+}
