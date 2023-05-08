72681d537ba7d2c8c316a26aeec980598ef9c8a3
davoodf1995 <davoodf1995@users.noreply.github.com>
2023-05-08 07:37:32 +0000

    Fix styling

--- src/Http/Controllers/PFWizardController.php
+++ src/Http/Controllers/PFWizardController.php
@@ -10,11 +10,11 @@ class PFWizardController extends Controller
     public function index(Request $request)
     {
         //Todo:: fix this :for getting $wizard
-        $pf_wizard = PFWizard::where('id',1)->where('status','active')->first();
-        if(isset($pf_wizard)){
-         $pf_steps = $pf_wizard->steps()->where('status','active')->orderBY('order')->get();
+        $pf_wizard = PFWizard::where('id', 1)->where('status', 'active')->first();
+        if (isset($pf_wizard)) {
+            $pf_steps = $pf_wizard->steps()->where('status', 'active')->orderBY('order')->get();
         }
-     
-        return view('productfinder::index',compact('pf_wizard','pf_steps'));
+
+        return view('productfinder::index', compact('pf_wizard', 'pf_steps'));
     }
 }
