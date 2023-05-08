40e108c1bcb7f47647226d6ae5aa57ceca91d592
davood.thinker <davood.thinker@gmail.com>
2023-05-08 18:24:22 +0330

    fix steps

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,7 +19,7 @@
         <div class="main">
         
             <div class="container">
-                <form method="POST" id="pfwizrd" class="signup-form" action="#">
+                <form method="POST" id="signup-form" class="signup-form" action="#">
                     <div>
                       @foreach ($pf_steps as $pf_step )
                         <h3>{{$pf_step->title}}</h3>
@@ -30,18 +30,24 @@
 
                             @foreach ($pf_step->questions as $pf_question)
                             <div class="form-row">
-                                <h4>{{$pf_question->title}}<span class="required">{{ $pf_question->is_required ? ' * ' : '' }}</span></h4>
+                                <h4>{{$pf_question->title}}</h4>
                                 @if ($pf_question->typeOption?->type?->name === 'checkbox')
                                 @foreach($pf_question->options as $value => $label)
-                               {{-- checkbox code --}}
+                                <label>
+                                    <input type="radio" name="age_gender" value="under_30_male">
+                                    <span class="radio-text">Under 30 and male</span>
+                                </label>
                                 @endforeach
                                 @endif
 
                                 @if ($pf_question->typeOption?->type?->name === 'radio')
                                 @foreach ($pf_question->options as $option)
+                                @php
+                                    $option_value=json_decode($option->value);
+                                @endphp
                                     <label>
                                         <input type="radio" name="{{ $pf_question->id }}" value="{{ $option->id }}">
-                                        <span class="radio-text">{{ json_decode($option->value)->title }}</span>
+                                        <span class="radio-text">{{ $option_value->title }}</span>
                                     </label>
                                 @endforeach
                                 @endif
@@ -57,7 +63,6 @@
                            
                             </div>
                         </fieldset>
-                      
                        @endforeach
                        <h3>Best Product for you!</h3>
                        <fieldset>
@@ -109,7 +114,14 @@
                                    </div>
                                </div>
                                <hr>
-                              
+                               <div class="form-group">
+                                   <label for="find_bank" class="form-label">Search your product!</label>
+                                   <div class="form-find">
+                                       <input type="text" name="find_bank" id="find_bank" placeholder="Ex. anti hair loss shampoo" />
+                                       <input type="submit" value="Search" class="submit">
+                                       <span class="form-icon"><i class="zmdi zmdi-search"></i></span>
+                                   </div>
+                               </div>
    
                            </div>
                        </fieldset>
@@ -161,6 +173,7 @@
    
                            </div>
                        </fieldset>
+
                     </div>
                 </form>
             </div>
@@ -176,46 +189,6 @@
     <script src="{{ asset('vendor/productfinder/lib/nouislider/nouislider.min.js')}}"></script>
     <script src="{{ asset('vendor/productfinder/lib/wnumb/wNumb.js')}}"></script>
     <script src="{{ asset('vendor/productfinder/js/main.js')}}"></script>
-
-    <script>
-  // Array of required questions for each step
-  const requiredQuestions = {
-    step1: ['1', '2'],
-    step2: ['6', ]
-  };
-
-// Function to check if all required questions have been answered
-function areRequiredQuestionsAnswered(step) {
-  const requiredQuestionsForStep = requiredQuestions[step];
-  let allAnswered = true;
-  requiredQuestionsForStep.forEach((questionId) => {
-    const radioButton = document.querySelector(`input[name=${questionId}]:checked`);
-    if (!radioButton) {
-      allAnswered = false;
-      // Display error message for unanswered question
-      const errorMessage = `Please answer question ${questionId}`;
-      const errorElement = document.getElementById(`${questionId}-error`);
-      errorElement.innerText = errorMessage;
-    }
-  });
-  return allAnswered;
-}
-
-// Function to handle "next" button click
-function onNextButtonClick() {
-  const currentStep = getCurrentStep();
-  const allRequiredQuestionsAnswered = areRequiredQuestionsAnswered(currentStep);
-  if (allRequiredQuestionsAnswered) {
-    // Proceed to next step
-    goToNextStep();
-  } else {
-    // Don't proceed to next step
-    return;
-  }
-}
-
-
-    </script>
 </body>
 
 </html>
